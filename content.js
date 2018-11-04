ebc7921c1070fe4c48a99864a9a49308ed189061
Jacky Zhu <jackyzhu@berkeley.edu>
2018-11-03 19:14:02 -0700

    added function to get rootDomain from URL

--- content.js
+++ content.js
@@ -1,2 +1,20 @@
 // content.js
 alert("Hello from your Chrome extension!")
+
+
+
+function rootDomain(url) {
+    splittedUrl = url.split("//")
+    url = splittedUrl[splittedUrl.length - 1]
+    var rightPeriodIndex;
+    for (var i = url.length - 1; i >= 0; i--) {
+        if(url[i] == '.') {
+            rightPeriodIndex = i;
+            var noExtension = url.substr(0,i);
+            break;
+        }
+    }
+    var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
+    return result;
+    
+  }
