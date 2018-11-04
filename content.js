b69623da1514ed8630bce59e99d3122e10b28db7
sxu1671 <sxu1671@gmail.com>
2018-11-03 20:14:07 -0700

    trying to import other js files:

--- content.js
+++ content.js
@@ -1,20 +1,24 @@
-// content.js
-alert("Hello from your Chrome extension!")
+var currPageURL = window.location.href;
+
+// function rootDomain(url) {
+//     splittedUrl = url.split("//")
+//     url = splittedUrl[splittedUrl.length - 1]
+//     var rightPeriodIndex;
+//     for (var i = url.length - 1; i >= 0; i--) {
+//         if(url[i] == '.') {
+//             rightPeriodIndex = i;
+//             var noExtension = url.substr(0,i);
+//             break;
+//         }
+//     }
+//     var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
+//     return result;
+// }
+
+alert(rootDomain(currPageURL))
+
+//     alert(rootDomain(currPageURL))
+// }
 
 
-
-function rootDomain(url) {
-    splittedUrl = url.split("//")
-    url = splittedUrl[splittedUrl.length - 1]
-    var rightPeriodIndex;
-    for (var i = url.length - 1; i >= 0; i--) {
-        if(url[i] == '.') {
-            rightPeriodIndex = i;
-            var noExtension = url.substr(0,i);
-            break;
-        }
-    }
-    var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
-    return result;
-    
-  }
+// alert("Hello from your Chrome extension!")
