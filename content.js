8d94bdc0c958504097ca30aa3ebf8044c446cc6b
Jacky Zhu <jackyzhu@berkeley.edu>
2018-11-03 20:59:13 -0700

    redid rootDomain

--- content.js
+++ content.js
@@ -7,18 +7,11 @@ if (domain == "amazon") {
 }
 
 function rootDomain(url) {
-    splittedUrl = url.split("//")
-    url = splittedUrl[splittedUrl.length - 1]
-    var rightPeriodIndex;
-    for (var i = url.length - 1; i >= 0; i--) {
-        if(url[i] == '.') {
-            rightPeriodIndex = i;
-            var noExtension = url.substr(0,i);
-            break;
-        }
-    }
-    var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
-    return result;
+    var urlObj = new URL(url)
+    var hostName = urlObj.hostname
+    var splittedHost = hostName.split(".")
+    var root = splittedHost[splittedHost.length - 2]
+    return root
 }
 
 function isConfirmation(url, identifier) {
