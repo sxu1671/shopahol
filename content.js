6a2321cdae33741c8770163e28791d7e1fd735fd
sxu1671 <sxu1671@gmail.com>
2018-11-03 20:47:51 -0700

    added check if its amazon confirmation

--- content.js
+++ content.js
@@ -1,24 +1,26 @@
 var currPageURL = window.location.href;
+var domain = rootDomain(currPageURL)
 
-// function rootDomain(url) {
-//     splittedUrl = url.split("//")
-//     url = splittedUrl[splittedUrl.length - 1]
-//     var rightPeriodIndex;
-//     for (var i = url.length - 1; i >= 0; i--) {
-//         if(url[i] == '.') {
-//             rightPeriodIndex = i;
-//             var noExtension = url.substr(0,i);
-//             break;
-//         }
-//     }
-//     var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
-//     return result;
-// }
+if (domain == "amazon") {
+    identifier = "purchaseId"
+    window.alert(isConfirmation(currPageURL, identifier));
+}
 
-alert(rootDomain(currPageURL))
+function rootDomain(url) {
+    splittedUrl = url.split("//")
+    url = splittedUrl[splittedUrl.length - 1]
+    var rightPeriodIndex;
+    for (var i = url.length - 1; i >= 0; i--) {
+        if(url[i] == '.') {
+            rightPeriodIndex = i;
+            var noExtension = url.substr(0,i);
+            break;
+        }
+    }
+    var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
+    return result;
+}
 
-//     alert(rootDomain(currPageURL))
-// }
-
-
-// alert("Hello from your Chrome extension!")
+function isConfirmation(url, identifier) {
+    return url.includes(identifier);
+}
