37badd70fbacafdb6ec58de3591de56191edfe00
sxu1671 <sxu1671@gmail.com>
2018-11-03 23:32:32 -0700

    comment out unused files

--- amazon.js
+++ amazon.js
@@ -1,3 +1,3 @@
-function isConfirmation(url) {
-    
-}
+// function isConfirmation(url) {
+
+// }
--- util.js
+++ util.js
@@ -1,14 +1,14 @@
-function rootDomain(url) {
-    splittedUrl = url.split("//")
-    url = splittedUrl[splittedUrl.length - 1]
-    var rightPeriodIndex;
-    for (var i = url.length - 1; i >= 0; i--) {
-        if(url[i] == '.') {
-            rightPeriodIndex = i;
-            var noExtension = url.substr(0,i);
-            break;
-        }
-    }
-    var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
-    return result;
-}
+// function rootDomain(url) {
+//     splittedUrl = url.split("//")
+//     url = splittedUrl[splittedUrl.length - 1]
+//     var rightPeriodIndex;
+//     for (var i = url.length - 1; i >= 0; i--) {
+//         if(url[i] == '.') {
+//             rightPeriodIndex = i;
+//             var noExtension = url.substr(0,i);
+//             break;
+//         }
+//     }
+//     var result = noExtension.substring(noExtension.lastIndexOf(".") + 1);
+//     return result;
+// }
